c148ab09c866ccbfd409da31c86f27efabca0314
Luca <luca.cavagnoli@gmail.com>
2011-12-06 19:02:04 +0100

    Added some fields to User class

--- grails-app/domain/giveitback/User.groovy
+++ grails-app/domain/giveitback/User.groovy
@@ -2,7 +2,14 @@ package giveitback
 
 import giveitback.security.SecUser
 
+/*
+ * This is my User class.
+ * 
+ * Do NOT update/create SecUser!
+ */
 class User extends SecUser {
 	String firstName
 	String lastName
+	Date dateCreated // GORM will update this automatically
+	Date lastUpdated // GORM will update this automatically
 }
