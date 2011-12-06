35c66e92f8a3df1fabe24c21e1c5a6c3b6e45c48
Luca <luca.cavagnoli@gmail.com>
2011-12-06 19:40:27 +0100

    Created Item and Loan classes

--- grails-app/domain/giveitback/Loan.groovy
+++ grails-app/domain/giveitback/Loan.groovy
@@ -1,17 +1,24 @@
 package giveitback
 
 import java.util.Date;
+import giveitback.User
+import giveitback.Person
+import giveitback.Item
 
 /*
- * This class is an Item that you borrow or lend
+ * a User lends to / borrows from a Person an Item
  */
-class Item {
+class Loan {
 	
-	String name
+	User user
+	Person person
+	Item item
 	String description
+	Date dueDate
 	Date dateCreated // GORM will update this automatically
 	Date lastUpdated // GORM will update this automatically
 
     static constraints = {
+		dueDate(nullable:true)
     }
 }
